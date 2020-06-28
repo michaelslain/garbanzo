cebea61f2cf93a48ba2a4ee9a1c81f7f61d4d8a2
michaelslain <misha.slain@icloud.com>
2020-06-28 14:19:46 -0700

    Fixed routing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './App.scss'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import axios from 'axios'
 import Storage from './cartApi'
 
