08066068884205366dc513ab9fd40e2dd344020b
michaelslain <misha.slain@icloud.com>
2020-06-28 14:32:18 -0700

    Fixed no products in database managment errors

--- client/src/containers/shop.jsx
+++ client/src/containers/shop.jsx
@@ -1,21 +1,29 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import './shop.scss'
 
 import LoadingScreen from '../components/loadingScreen'
 import Product from '../components/product'
 
 export default function Shop({ products, canProduct }) {
+    if (!canProduct) return <LoadingScreen />
+
+    if (products.length === 1) {
+        return (
+            <div className="no-products-container">
+                <i>
+                    <h1 className="text">
+                        There are currently no products in store
+                    </h1>
+                </i>
+            </div>
+        )
+    }
+
     return (
-        <Fragment>
-            {!canProduct ? (
-                <LoadingScreen />
-            ) : (
-                <div className="shop-showcase">
-                    {products.map(data => (
-                        <Product key={data._id} data={data} />
-                    ))}
-                </div>
-            )}
-        </Fragment>
+        <div className="shop-showcase">
+            {products.map(data => (
+                <Product key={data._id} data={data} />
+            ))}
+        </div>
     )
 }
